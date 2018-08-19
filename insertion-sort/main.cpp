77ffeb3bba7f9c7a46e477dc3088aa722af22175
kristofferhebert <kristofferhebert>
2018-08-18 18:12:55 -0700

    Completing insert sort

--- insertion-sort/main.cpp
+++ insertion-sort/main.cpp
@@ -1,39 +1,20 @@
 #include <iostream>
 using namespace std;
 
-void swap_values(int *arr, int left, int right){
-  int tmp = arr[left];
-  arr[left] = arr[right];
-  arr[right] = tmp;
-  return;
-}
-
 void print_array(int *arr, int size){
   for(int i = 0; i < size; i++){
     cout << arr[i] << endl;
   }
 }
 
-void insert(int *arr, int begin, int end){
-  bool finish = false;
-  int current_index = end;
-  bool more = (current_index != begin);
-  while (more && !finish){
-    if(arr[current_index] < arr[current_index - 1]){
-      cout << "started" << endl;
-      swap_values(arr, begin, end -1);
-      current_index--;
-      more = (current_index != begin);
-    } else {
-      cout << "finished" << endl;
-      finish = true;
-    }
-  }
-  return;
-}
 void insertion_sort(int *arr, int size){
-  for(int i = 0; i < size; i++){
-    insert(arr, 0, i);
+  for(int i = 0; i < size; i ++){
+    int tmp;
+    for (int j = i; 0 < j && (arr[j] < arr[j - 1]); j--) {
+        tmp = arr[j];
+        arr[j] = arr[j - 1];
+        arr[j - 1] = tmp;
+    }
   }
 }
 
